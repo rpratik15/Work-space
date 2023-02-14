dc1f1c4113901364a2df6003a064e20be51f5bbe
Pratik Raval <rpratik15@gmail.com>
2023-02-14 17:03:52 +0530

    update1

---
+++ work-place-app/src/Components/Auth/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./auth.css"
+
+function index() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
---
+++ work-place-app/src/Components/LandingPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import TopNav from './TopNav'
+import RightJobSection from './RightJobSection'
+import ManySolutoinSection from './ManySolutoinSection'
+function LandingPage() {
+  return (
+    <div>
+      <TopNav />
+      <RightJobSection/>
+      <ManySolutoinSection/>
+      
+    </div>
+  )
+}
+
+export default LandingPage
